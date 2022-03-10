c2441c6135b9342c51458a1aa9fb97340df36a56
huy-axie <huy@axieinfinity.com>
2022-03-10 15:36:40 +0700

    add origin

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module game-server-exporter
 
 go 1.17
 
-require (
-	github.com/gorilla/websocket v1.4.2
-	github.com/prometheus/client_golang v1.11.0
-)
+require github.com/prometheus/client_golang v1.11.0
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
-	"strconv"
 	"time"
 
-	"github.com/gorilla/websocket"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-const bitSize = 64
+type Data struct {
+	Total int `json:"total"`
+}
 
-var battles float64
-var done chan interface{}
-var interrupt chan os.Signal
+var battles int
+
 var (
 	battlesNumber = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:        "game_server_battle_total",
+		Name:        "origin_game_server_battle_total",
 		Help:        "The total number of match happening",
 		ConstLabels: map[string]string{"nodename": getHostName()},
 	})
@@ -31,72 +31,45 @@ var (
 func recordMetrics() {
 	go func() {
 		for {
-			battlesNumber.Set(battles)
-			time.Sleep(30 * time.Second)
+			// get battles
+			getBattles()
+			// set metrics
+			battlesNumber.Set(float64(battles))
+			time.Sleep(15 * time.Second)
 		}
 	}()
 }
 
-// received number battles
-func receiveHandler(connection *websocket.Conn) {
-	defer close(done)
-	for {
-		_, msg, err := connection.ReadMessage()
-		if err != nil {
-			log.Println("Error in receive:", err)
-			return
-		}
-		battles, _ = strconv.ParseFloat(string(msg), bitSize)
-	}
-}
-
 func getBattles() {
-	done = make(chan interface{})    // Channel to indicate that the receiverHandler is done
-	interrupt = make(chan os.Signal) // Channel to listen for interrupt signal to terminate gracefully
+	httpUrl := "http://" + os.Getenv("GAME_SERVER_IP") + ":" + os.Getenv("GAME_SERVER_PORT") + "/" + os.Getenv("BATTLE_PATH")
+	// fmt.Println("Open connection to " + httpUrl)
 
-	signal.Notify(interrupt, os.Interrupt) // Notify the interrupt channel for SIGINT
-
-	socketUrl := "ws://" + os.Getenv("GAME_SERVER_IP") + ":" + os.Getenv("GAME_SERVER_PORT") + "/" + os.Getenv("WS_PATH")
-	fmt.Println("Open connection to " + socketUrl)
-	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
+	req, err := http.NewRequest("GET", httpUrl, nil)
 	if err != nil {
-		log.Fatal("Error connecting to Websocket Server:", err)
+		panic(err)
 	}
-	defer conn.Close()
-	go receiveHandler(conn)
 
-	// Our main loop for the client
-	// We send our relevant packets here
-	for {
-		select {
-		case <-time.After(time.Duration(1) * time.Second * 30):
-			// Send an echo packet every second
-			err := conn.WriteMessage(websocket.TextMessage, []byte(os.Getenv("GAME_SERVER_TOKEN")))
-			if err != nil {
-				log.Println("Error during writing to websocket:", err)
-				return
-			}
-
-		case <-interrupt:
-			// We received a SIGINT (Ctrl + C). Terminate gracefully...
-			log.Println("Received SIGINT interrupt signal. Closing all pending connections")
-
-			// Close our websocket connection
-			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("Error during closing websocket:", err)
-				return
-			}
-
-			select {
-			case <-done:
-				log.Println("Receiver Channel Closed! Exiting....")
-			case <-time.After(time.Duration(1) * time.Second):
-				log.Println("Timeout in closing receiving channel. Exiting....")
-			}
-			return
-		}
+	req.Header = http.Header{
+		"Content-Type":  []string{"application/json"},
+		"Authorization": []string{os.Getenv("GAME_SERVER_TOKEN")},
 	}
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	dat := Data{}
+	if err := json.Unmarshal(body, &dat); err != nil {
+		panic(err)
+	}
+
+	battles = dat.Total
 }
 
 // Get node name
@@ -111,9 +84,6 @@ func getHostName() string {
 func main() {
 	// register metrics
 	prometheus.MustRegister(battlesNumber)
-
-	// start to count battle
-	go getBattles()
 
 	// record metrics
 	recordMetrics()
